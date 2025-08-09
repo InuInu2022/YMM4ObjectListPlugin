11c018d18466fff1397fd4a47fdd5d1685cc4cb6
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2025-08-09 10:45:27 +0900

    feat: CategoryFilterPanelにPanelStyleプロパティを追加し、MainViewでCompactスタイルを適用

--- src/YMM4ObjectListPlugin/View/CategoryFilterPanel.xaml.cs
+++ src/YMM4ObjectListPlugin/View/CategoryFilterPanel.xaml.cs
@@ -9,6 +9,20 @@ namespace ObjectList.View;
 /// </summary>
 public partial class CategoryFilterPanel : UserControl
 {
+	public static readonly DependencyProperty PanelStyleProperty =
+		DependencyProperty.Register(
+			nameof(PanelStyle),
+			typeof(string),
+			typeof(CategoryFilterPanel),
+			new PropertyMetadata("Default")
+		);
+
+	public string PanelStyle
+	{
+		get => (string)GetValue(PanelStyleProperty);
+		set => SetValue(PanelStyleProperty, value);
+	}
+
 	public CategoryFilterPanel()
 	{
 		InitializeComponent();
