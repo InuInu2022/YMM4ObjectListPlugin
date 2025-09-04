bde8f28abc0a6272fe7a5a09fd49ac760e67a35a
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2025-09-05 04:21:46 +0900

    fix: add NLog.Extensions.Logging package and implement logging configuration in Ymm4ObjectListPlugin

--- src/YMM4ObjectListPlugin/YMM4ObjectListPlugin.cs
+++ src/YMM4ObjectListPlugin/YMM4ObjectListPlugin.cs
@@ -3,11 +3,23 @@ using System.Reflection;
 using System.Text.Json;
 using System.Windows;
 using System.Windows.Threading;
+
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using NLog.Extensions.Logging;
 using ObjectList.View;
 using ObjectList.ViewModel;
-
 using YmmeUtil.Bridge;
+using YmmeUtil.Ymm4;
 using YukkuriMovieMaker.Plugin;
+using NLog.Config;
+using System.IO;
+using YukkuriMovieMaker.Commons;
+using NLog.Targets;
+using NLog.Targets.Wrappers;
+using NLog;
 
 namespace ObjectList;
 
@@ -15,24 +27,59 @@ namespace ObjectList;
 	AuthorName = "InuInu",
 	ContentId = "nc424814"
 )]
-public class Ymm4ObjectListPlugin : IToolPlugin
+public class Ymm4ObjectListPlugin : IToolPlugin, IDisposable
 {
 	public string Name { get; } = "YMM4 オブジェクトリスト";
 	public PluginDetailsAttribute Details =>
-		GetType().GetCustomAttribute<PluginDetailsAttribute>() ?? new();
+		GetType()
+			.GetCustomAttribute<PluginDetailsAttribute>()
+		?? new();
 
-	public Type ViewModelType { get; } = typeof(MainViewModel);
+	public Type ViewModelType { get; } =
+		typeof(MainViewModel);
 	public Type ViewType { get; } = typeof(MainView);
 
-	private static readonly JsonSerializerOptions _jsonOptions = new() { WriteIndented = true };
+	private static readonly JsonSerializerOptions _jsonOptions =
+		new() { WriteIndented = true };
+	private readonly PluginErrorHandler _monitor;
+
+	static readonly DisposeCollector disposer = new();
+	private bool _disposedValue;
 
 	public Ymm4ObjectListPlugin()
 	{
-		var timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(500) };
+		//DI
+		SetLogging();
+		var services = new ServiceCollection();
+		services.AddLogging(builder =>
+		{
+			builder.ClearProviders();
+			builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Warning);
+			builder.AddNLog();
+		});
+		services.AddTransient<Ymm4ObjectListPlugin>();
+		using var provider = services.BuildServiceProvider();
+
+		//プラグイン由来のエラー処理
+		var logger = provider.GetRequiredService<
+			ILogger<Ymm4ObjectListPlugin>
+		>();
+		_monitor = PluginErrorHandler.Initialize(
+			typeof(Ymm4ObjectListPlugin).Assembly,
+			logger
+		);
+
+		//UI load
+		var timer = new DispatcherTimer
+		{
+			Interval = TimeSpan.FromMilliseconds(500),
+		};
 
 		void TickEvent(object? s, EventArgs e)
 		{
-			foreach (Window win in Application.Current.Windows)
+			foreach (
+				Window win in Application.Current.Windows
+			)
 			{
 				// スプラッシュではなく、実ウィンドウかを判定
 				if (IsRealUiWindow(win) && win.IsLoaded)
@@ -49,18 +96,108 @@ public class Ymm4ObjectListPlugin : IToolPlugin
 		timer.Start();
 	}
 
+	static void SetLogging()
+	{
+		var config = new LoggingConfiguration();
+
+		// ファイルターゲットの設定
+		var logDir = Path.Combine(
+			AppDirectories.LogDirectory,
+			"Ymm4ObjectListPlugin",
+			"logs"
+		);
+		Directory.CreateDirectory(logDir);
+
+		var fileTarget = new FileTarget("logfile")
+		{
+			FileName = $"{logDir}/app_${{shortdate}}.log",
+			ArchiveFileName =
+				$"{logDir}/archive/app.log",
+			ArchiveEvery = FileArchivePeriod.Day,
+			ArchiveSuffixFormat = "_{#}",
+			MaxArchiveFiles = 30,
+			KeepFileOpen = false,
+			Encoding = System.Text.Encoding.UTF8,
+			Layout =
+				"${longdate} [${level:uppercase=true}] [${logger}] ${message} ${exception:format=tostring}",
+		};
+		disposer.Collect(fileTarget);
+
+		// 非同期ラッパー
+		var asyncFileTarget = new AsyncTargetWrapper(
+			fileTarget,
+			5000,
+			AsyncTargetWrapperOverflowAction.Discard
+		)
+		{
+			Name = "asyncFile",
+		};
+		disposer.Collect(asyncFileTarget);
+
+		config.AddTarget(asyncFileTarget);
+
+		// ルールの追加
+		config.AddRule(
+			NLog.LogLevel.Error,
+			NLog.LogLevel.Fatal,
+			asyncFileTarget
+		);
+
+		// 設定を適用
+		LogManager.Configuration = config;
+	}
+
 	private static bool IsRealUiWindow(Window window)
 	{
-		return !string.IsNullOrWhiteSpace(window.Title) && window.Title != "Splash";
+		return !string.IsNullOrWhiteSpace(window.Title)
+			&& !string.Equals(
+				window.Title,
+				"Splash",
+				StringComparison.Ordinal
+			);
 	}
 
 	private static void OnUiReady(Window mainWindow)
 	{
 		// 本体UIにアクセス可能
 		//test
-		var hasTL = TimelineUtil.TryGetTimeline(out var timeLine);
+		var hasTL = TimelineUtil.TryGetTimeline(
+			out var timeLine
+		);
 		if (!hasTL || timeLine is null)
 			return;
-		Debug.WriteLine(JsonSerializer.Serialize(timeLine.VideoInfo, _jsonOptions));
+		Debug.WriteLine(
+			JsonSerializer.Serialize(
+				timeLine.VideoInfo,
+				_jsonOptions
+			)
+		);
+	}
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!_disposedValue)
+		{
+			if (disposing)
+			{
+				disposer.DisposeAndClear();
+			}
+
+			_disposedValue = true;
+		}
+	}
+
+	// // TODO: 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
+	// ~Ymm4ObjectListPlugin()
+	// {
+	//     // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+	//     Dispose(disposing: false);
+	// }
+
+	public void Dispose()
+	{
+		// このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
 	}
 }
